e320c1078247e4f7f0a6f0697aee569bdad0f313
Elliot Chung <elliotmch@yahoo.com>
2018-01-23 20:58:14 -0500

    CommandBase Adjustment
    Subsystem "OurCompressor" was added to the CommandBase.

--- src/org/usfirst/frc/team3015/robot/commands/CommandBase.java
+++ src/org/usfirst/frc/team3015/robot/commands/CommandBase.java
@@ -4,6 +4,7 @@ import org.usfirst.frc.team3015.robot.OI;
 import org.usfirst.frc.team3015.robot.subsystems.Drive;
 import org.usfirst.frc.team3015.robot.subsystems.Grabber;
 import org.usfirst.frc.team3015.robot.subsystems.Intake;
+import org.usfirst.frc.team3015.robot.subsystems.OurCompressor;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -12,11 +13,13 @@ public abstract class CommandBase extends Command {
 	public static OI oi;
 	public static Intake intake; 
 	public static Grabber grabber;
+	public static OurCompressor ourCompressor;
 	
 	public static void init() {
 		drive = new Drive();
 		intake = new Intake();
 		grabber = new Grabber();
+		ourCompressor = new OurCompressor();
 
 		
 		
