2c5c5bfc58cb3f631e50dc132f38472063ddd0a3
Elliot Chung <elliotmch@yahoo.com>
2018-01-23 20:54:19 -0500

    OurCompressor
    Subsystem is completed. Commands for OurCompressor are in progress.

---
+++ src/org/usfirst/frc/team3015/robot/subsystems/OurCompressor.java
@@ -0,0 +1,46 @@
+package org.usfirst.frc.team3015.robot.subsystems;
+
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Compressor subsystem
+ * Prefixed with "Our" to prevent naming conflicts
+ */
+public class OurCompressor extends Subsystem {
+	private Compressor compressor;
+	private AnalogPotentiometer pressureSensor;
+	
+	public OurCompressor() {
+		compressor = new Compressor();
+		pressureSensor = new AnalogPotentiometer(0, 250, -25);
+	}
+	
+	/**
+	 * Make the compressor run based on the state of the pressure switch.
+	 */
+	public void startCompressor() {
+		compressor.start();
+	}
+	
+	/**
+	 * Stop the compressor from running
+	 */
+	public void stopCompressor() {
+		compressor.stop();
+	}
+	
+	/**
+	 * Get the pressure sensor's reading
+	 * @return Pressure reading in PSI
+	 */
+	public double getPressure() {
+		return pressureSensor.get();
+	}
+	
+    public void initDefaultCommand() {
+    	// Set the default command for a subsystem here.
+    	// setDefaultCommand(new OurCompressorAuto());
+    }
+}
